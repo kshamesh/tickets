4ee4f40d82d6ec3bca8ea3ba1a2256859459f383
Kshamesh <Kshamesh@gmail.com>
2025-02-02 20:06:59 +0530

    Mongo DB - Docker

--- src/main/java/com/pluralsight/springboot/tickets/registration/RegistrationController.java
+++ src/main/java/com/pluralsight/springboot/tickets/registration/RegistrationController.java
@@ -1,13 +1,13 @@
 package com.pluralsight.springboot.tickets.registration;
 
 import jakarta.validation.Valid;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.NoSuchElementException;
+import java.util.UUID;
 
 @RestController
-@RequestMapping(path="/registrations")
+@RequestMapping(path = "/registrations")
 public class RegistrationController {
 
     private final RegistrationRepository registrationRepository;
@@ -18,23 +18,32 @@ public class RegistrationController {
 
     @PostMapping
     public Registration create(@RequestBody @Valid Registration registration) {
-        return registrationRepository.create(registration);
+        String ticketCode = UUID.randomUUID().toString();
+
+        return registrationRepository.save(new Registration(
+                null, registration.productId(), ticketCode, registration.attendeeName()));
     }
 
-    @GetMapping
-    public Registration get(@RequestParam("ticketCode") String ticketCode) {
+    @GetMapping(path = "/{ticketCode}")
+    public Registration get(@PathVariable("ticketCode") String ticketCode) {
         return registrationRepository.findByTicketCode(ticketCode)
-                .orElseThrow(() -> new NoSuchElementException("Registration with ticket code " + ticketCode + "is not found"));
+                .orElseThrow(() -> new NoSuchElementException("Registration with ticket code " + ticketCode + " not found"));
     }
-
 
     @PutMapping
     public Registration update(@RequestBody Registration registration) {
-        return registrationRepository.update(registration);
+        // Lookup the existing registration by ticket code
+        String ticketCode = registration.ticketCode();
+        var existing = registrationRepository.findByTicketCode(ticketCode)
+                .orElseThrow(() -> new NoSuchElementException("Registration with ticket code " + ticketCode + " not found"));
+
+        // Only update the attendee name
+        return registrationRepository.save(new Registration(
+                existing.id(), existing.productId(), ticketCode, registration.attendeeName()));
     }
 
     @DeleteMapping(path = "/{ticketCode}")
-    public void delete(@RequestParam String ticketCode) {
+    public void delete(@PathVariable("ticketCode") String ticketCode) {
         registrationRepository.deleteByTicketCode(ticketCode);
     }
 }
